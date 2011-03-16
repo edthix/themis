6145d8a173e50b9e40b52f5f479a7cbacfc52aae
Edham Arief / edthix <edthix@gmail.com>
2011-03-16 21:15:43 +0800

    Add log to gitignore and changed db config

--- src/config/db_config.clj
+++ src/config/db_config.clj
@@ -3,42 +3,46 @@
 (ns config.db-config
   (:require [clojure.contrib.java-utils :as java-utils]
             [conjure.core.config.environment :as environment]
-            [conjure.core.db.flavors.h2 :as h2]))
+            [conjure.core.db.flavors.h2 :as h2]
+            [conjure.core.db.flavors.mysql :as mysql]))
 
 (defn
-#^{:doc "Returns the database config map which is used by jdbc_connector to connect to the database."}
+  #^{:doc "Returns the database config map which is used by jdbc_connector to connect to the database."}
   create-db-config 
   ([] (create-db-config :production))
   ([environment]
-    (let [
-          ;; The default settings for all environments
-          base-config
-            {
-              ;; The database flavor (SQL syntax type)
-              :flavor (h2/flavor)
-          
-              ;; The user name to use when connecting to the database.
-              :username "conjure"
+     (let [
+           ;; The default settings for all environments
+           base-config
+           {
+            ;; The database flavor (SQL syntax type)
+            :flavor (mysql/flavor)
             
-              ;; The password to use when connecting to the database.
-              :password "conjure" }]
+            ;; The user name to use when connecting to the database.
+            :username "root"
+            
+            ;; The password to use when connecting to the database.
+            :password ""
 
-      (cond
+            :host "10.0.0.1"
+            }]
+       
+       (cond
         (= environment :production)
-          (merge base-config { 
-            ;; The name of the production database to use.
-            :dbname "conjure_production" })
-            
+        (merge base-config { 
+                            ;; The name of the production database to use.
+                            :dbname "conjure_production" })
+        
         (= environment :development)
-          (merge base-config { 
-            ;; The name of the development database to use.
-            :dbname "conjure_development" })
-            
+        (merge base-config { 
+                            ;; The name of the development database to use.
+                            :dbname "clojure" })
+        
         (= environment :test)
-          (merge base-config { 
-            ;; The name of the test database to use.
-            :dbname "conjure_test" })))))
-            
+        (merge base-config { 
+                            ;; The name of the test database to use.
+                            :dbname "conjure_test" })))))
+
 (defn
   load-config []
   (let [environment (environment/environment-name)
@@ -46,4 +50,3 @@
     (if base-config
       (((:flavor base-config) :db-map) base-config)
       (throw (new RuntimeException (str "Unknown environment: " environment ". Please check your conjure.environment system property."))))))
-      
